39ff627b7431bd12365298d35b2f9876135ceab4
unknown <recfrag@gmail.com>
2019-09-09 18:43:48 +0300

    make actorlist

--- src/ApiService.js
+++ src/ApiService.js
@@ -3,10 +3,7 @@ export default class ApiService {
 
     async getResource(url) {
         const res = await fetch(`${this._baseURL}${url}`);
-
-        if(!res.ok) {
-            throw new Error(`not fetch ${url}` + `, received ${res.status}`)
-        }
+      
         return await res.json();
     }
     async getActors(){
@@ -15,11 +12,11 @@ export default class ApiService {
     }
 }
 
-const swapi = new ApiService();
+// const swapi = new ApiService();
 
-swapi.getActors().then((people)=>{
-    people.forEach(p => {
-        console.log(p.name);
-    });
-})
+// swapi.getActors().then((people)=>{
+//     people.forEach(p => {
+//         console.log(p.name);
+//     });
+// })
 
--- src/components/actors/actors.js
+++ src/components/actors/actors.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react';
+import ApiService from '../../ApiService';
 
-export default function Actors() {
-    return (
-        <div>
-            actors
-        </div>
-    )
+export default class Actors extends Component {
+
+    apiService = new ApiService();
+
+    state = {
+        actorsList: []
+    }
+
+    componentDidMount() {
+        this.apiService.getActors().then((actorsL) => {
+            this.setState({
+                actorsList: actorsL
+            })
+        })
+    }
+
+    renderActors(arr) {
+        return arr.map(({ name }) => {
+            return (
+                <li key={Math.random()}>
+                    {name}
+                </li>
+            )
+        })
+    }
+
+    render() {
+        const { actorsList } = this.state;
+        const actors = this.renderActors(actorsList);
+        console.log(this.state)
+        return (
+            <Fragment>
+                {actors}
+            </Fragment>
+        )
+    }
 }
