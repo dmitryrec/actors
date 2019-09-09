9ae4832a72ff2498f5ca7eb1be1d716441126303
unknown <recfrag@gmail.com>
2019-09-09 16:54:10 +0300

    begin

---
+++ src/ApiService.js
@@ -0,0 +1,25 @@
+export default class ApiService {
+    _baseURL = 'https://swapi.co/api';
+
+    async getResource(url) {
+        const res = await fetch(`${this._baseURL}${url}`);
+
+        if(!res.ok) {
+            throw new Error(`not fetch ${url}` + `, received ${res.status}`)
+        }
+        return await res.json();
+    }
+    async getActors(){
+       const res = await this.getResource(`/people/`);
+       return res.results;
+    }
+}
+
+const swapi = new ApiService();
+
+swapi.getActors().then((people)=>{
+    people.forEach(p => {
+        console.log(p.name);
+    });
+})
+
---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../header/header';
+
+const App = () => {
+    return (
+        <div>
+            <h1>fff</h1>
+        </div>
+    );
+}
+
+export default App;
