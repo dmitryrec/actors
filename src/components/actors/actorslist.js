4095393de40631b08845432a8ed9dece9894d3f5
unknown <recfrag@gmail.com>
2019-09-12 21:57:20 +0300

    final

--- src/components/actors/actorslist.js
+++ src/components/actors/actorslist.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ActorsList = ({ ...props }) => {
     return props.actorsList.map(({ name, url }) =>
-        <li key={url}>
-            {name}
-            <button onClick={() => props.onDelActor(url)}>del</button>
+        <li key={url} className="actorItem">
+              <span className="actorName">{name}</span> 
+            <button onClick={() => props.onDelActor(url)} className="actorButton">del</button>
         </li>)
 
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from '../header';
+import './app.css'
 
 const App = ({children}) => {
     return (
